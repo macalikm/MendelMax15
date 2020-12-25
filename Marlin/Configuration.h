f30b0a505a6d6cf7416050cc26d72e576e034bd1
macalikm <michael.macalik@gmail.com>
2020-12-25 00:04:09 -0500

    #define XY_PROBE_SPEED (30*60) = HOMING_FEEDRATE_XY (30*60)

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -988,7 +988,7 @@
 #define PROBING_MARGIN 10   //mlm 10
 
 // X and Y axis travel speed (mm/min) between probes
-#define XY_PROBE_SPEED (40*60)    //mlm default (133*60)
+#define XY_PROBE_SPEED (30*60)    //mlm default (133*60)
 
 // Feedrate (mm/min) for the first approach when double-probing (MULTIPLE_PROBING == 2)
 #define Z_PROBE_SPEED_FAST HOMING_FEEDRATE_Z
